5c2956c45264dc412fa9a34f712f30a1e16b9a07
nitishtalekar <nitishtalekar.nt503@gmail.com>
2024-10-15 10:35:50 -0400

    new id

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    CLIENT_ID: "0000000000000000000", // Discord Application ID
-    COMPANY_NAME: "TheSleepyKoala", // Company or Developer Name
+    CLIENT_ID: "1287782239924387891", // Discord Application ID
+    COMPANY_NAME: "Nitish", // Company or Developer Name
     PRODUCT_NAME: "Pong with GDScript", // Game Name
     PRODUCT_VERSION: "0.1.0", // Game Version
 };
